f4c05af4674e9dd68a9f652f6b1a7346cd679086
Henry Rapley <froggywillneverdie@msn.com>
2011-07-29 03:32:43 +0100

    created some files for oauth implementation

---
+++ dAmnViper/dA/oauth.py
@@ -0,0 +1,15 @@
+''' dAmnViper.dA.oauth module
+    Created by photofroggy.
+    
+    This module provides objects which can be used to authorize applications
+    with deviantart.com's oAuth API. Note that this product is in no way
+    affiliated with or endorsed by deviantART.com. This is not an official
+    service of deviantART.com. This is an independent project created by
+    photofroggy.
+'''
+
+
+
+
+
+# EOF
